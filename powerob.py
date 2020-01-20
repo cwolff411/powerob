f27ea6b853c108316129bfe9fa7854b622648ba0
Cory Wolff <cwolff411@gmail.com>
2020-01-19 22:18:22 -0500

    changed command line input setup. cleaned up code.

--- powerob.py
+++ powerob.py
@@ -39,6 +39,24 @@ def db_check():
         print("[*] Db file found")
         return True
 
+# Adds the new functions/obfuscated functions to the db file. e.g. filename => original function: obfuscated function
+def save_functions(filename, functions):
+
+    to_write = {}
+    to_write[filename] = functions
+
+    try:
+        with open('db.json', "w+") as file:
+            file.write(json.dumps(to_write))
+            file.close()
+            return True
+    except IOError as e:
+        errno, strerror = e.args
+        print("[-] I/O error ({}): {}\n[-] No file written".format(errno, strerror))
+    except: #handle other exceptions such as attribute errors
+        print("Unexpected error saving to db file: ", sys.exc_info()[0])
+    pass
+
 def find_functions(input):
     print("[+] Loading the Powershell script")
 
@@ -85,7 +103,6 @@ def create_obfuscated_functions(functions):
 
     return substitutions
 
-
 def write_obfuscated_file(inputfile, outputfile, functions):
 
     try:
@@ -110,101 +127,107 @@ def write_obfuscated_file(inputfile, outputfile, functions):
         exit()
     pass
 
-# Adds the new functions/obfuscated functions to the db file. e.g. filename => original function: obfuscated function
-def save_functions(filename, functions):
+class PowerOb(object):
 
-    to_write = {}
-    to_write[filename] = functions
-    print (json.dumps(to_write))
-    try:
-        with open('db.json', "w+") as file:
-            file.write(json.dumps(to_write))
-            file.close()
-            return True
-    except IOError as e:
-        errno, strerror = e.args
-        print("[-] I/O error ({}): {}\n[-] No file written".format(errno, strerror))
-    except: #handle other exceptions such as attribute errors
-        print("Unexpected error saving to db file: ", sys.exc_info()[0])
-    pass
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='Pretends to be git',usage='''powerob.py <command> [<args>]
+            Possible Commands:
+            obfuscate - Obfuscate new powershell script
+            list - List obfuscated files and their commands
+            getcommand - Get a particular command from an obfuscated file
 
-def obfuscate_functions(inputfile, outputfile):
-
-    # Find the functions. Returns list of functions in the inputfile
-    functions = find_functions(inputfile)
-
-    # Generate random strings. This is where substitutions{} is populated. Returns list. eg. [original_function, obfuscated_function]
-    obfuscated_functions = create_obfuscated_functions(functions)
-
-    obfuscated_file = write_obfuscated_file(inputfile, outputfile, obfuscated_functions)
-
-    if obfuscated_file:
-
-        print("\n" + dashline)
-        print('{:<30s}{:>30s}'.format("Original Function","Obfuscated Function"))
-        print(dashline)
-        
-        for functions in obfuscated_functions.items():
-            print('{:<30}{:>25}'.format(functions[0],functions[1]))
-
-        save_db = save_functions(outputfile, obfuscated_functions)
-        if save_db:
-            print("\n[+] Saving to db file")
-        print("[+] Done. Output file located at " + outputfile)
+            Try powerob.py <command> -h for help with a particular command.
+            ''')
+        parser.add_argument('command', help='Command to run')
+        # parse_args defaults to [1:] for args, but you need to
+        # exclude the rest of the args too, or validation will fail
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print ('Unrecognized command')
+            parser.print_help()
+            exit(1)
+        # use dispatch pattern to invoke method with same name
+        getattr(self, args.command)()
 
 
-def show_obfuscated_files():
-    try:
-        with open('db.json') as file:
-            g = json.load(file)
+    def obfuscate(self):
+        parser = argparse.ArgumentParser(description='Obfuscate a new Powershell script.')
+        # prefixing the argument with -- means it's optional
+        parser.add_argument('inputfile', type=str)
+        parser.add_argument('outputfile', type=str)
 
-            if file == None:
-                print ("[-] No obfuscated files found!")
+        db_check()
+
+        args = parser.parse_args(sys.argv[2:])
+
+
+        # Find the functions. Returns list of functions in the inputfile
+        functions = find_functions(args.inputfile)
+
+        # Generate random strings. This is where substitutions{} is populated. Returns list. eg. [original_function, obfuscated_function]
+        obfuscated_functions = create_obfuscated_functions(functions)
+
+        obfuscated_file = write_obfuscated_file(args.inputfile, args.outputfile, obfuscated_functions)
+
+        if obfuscated_file:
+
+            print("\n" + dashline)
+            print('{:<30s}{:>30s}'.format("Original Function","Obfuscated Function"))
+            print(dashline)
             
-            for filename, funcs in g.items():
+            for functions in obfuscated_functions.items():
+                print('{:<30}{:>25}'.format(functions[0],functions[1]))
 
-                print("\n" + dashline)
-                print('Obfuscated File: ' + filename)
+            save_db = save_functions(args.outputfile, obfuscated_functions)
+            if save_db:
+                print("\n[+] Saving to db file")
+            print("[+] Done. Output file located at " + args.outputfile)
 
-                print('{:<30s}{:>30s}'.format("Original Function","Obfuscated Function"))
-                print(dashline)
+    def list(self):
+        parser = argparse.ArgumentParser(description='List all obfuscated files and their commands.')
 
-                for f in funcs.items():
-                    print('{:<30}{:>25}'.format(f[0],f[1]))       
-                print(dashline)
-        print("[+] Done")
-    except IOError as e:
-        errno, strerror = e.args
-        if errno == 2:
-            print('[-] No obfuscated files found!')
-        else:
-            print("[-] I/O error ({}): {}".format(errno, strerror))
-    except: #handle other exceptions such as attribute errors
-        print("Unexpected error opening db file: ", sys.exc_info()[0])
-    pass
+        try:
+            with open('db.json') as file:
+                g = json.load(file)
+
+                if file == None:
+                    print ("[-] No obfuscated files found!")
+                
+                for filename, funcs in g.items():
+
+                    print("\n" + dashline)
+                    print('Obfuscated File: ' + filename)
+
+                    print('{:<30s}{:>30s}'.format("Original Function","Obfuscated Function"))
+                    print(dashline)
+
+                    for f in funcs.items():
+                        print('{:<30}{:>25}'.format(f[0],f[1]))       
+                    print(dashline)
+            print("[+] Done")
+
+        except IOError as e:
+            errno, strerror = e.args
+            if errno == 2:
+                print('[-] No obfuscated files found!')
+            else:
+                print("[-] I/O error ({}): {}".format(errno, strerror))
+        except: #handle other exceptions such as attribute errors
+            print("Unexpected error opening db file: ", sys.exc_info()[0])
+        pass
+
+
+    def getcommand(self):
+        parser = argparse.ArgumentParser(description='Get a particular command from an obfuscated file.')
+        parser.add_argument('file', type=str, help='The name of the obfuscated file.')
+        parser.add_argument('scriptcommand', type=str, help='The name of the command from the original script that you would like the obfuscated equivalent.')
+        args= parser.parse_args(sys.argv[2:])
+
 
 
 if __name__ == '__main__':
 
     banner()
     python_check()
-    db_check()
 
-    parser = argparse.ArgumentParser(description='Powershell Script Obfuscator')
-    parser.add_argument('-a', '--action', required=True, type=str, help="The action you would like to take.")
-    parser.add_argument('-i', '--inputfile', type=str, help='Location of the Powershell script to obfuscate')
-    parser.add_argument('-o','--outputfile', type=str, help='Path and filename of the output script')
-    args = parser.parse_args()
-
-
-    if args.action == "list":
-        show_obfuscated_files()
-    elif args.action == "get-command":
-        print('get-command')
-        exit()
-    elif args.action == "obfuscate":
-        if args.inputfile == None or args.outputfile == None:
-            print("[-] Please provide an input file and an output file")
-            exit()
-        else:
-            obfuscate_functions(args.inputfile, args.outputfile)
+    PowerOb()
